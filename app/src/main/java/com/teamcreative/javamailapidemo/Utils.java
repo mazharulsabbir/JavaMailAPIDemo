6a025b9b04822e1aa5bc54b0e61f6b000784f57b
mazharulsabbir <mazharulsabbir2@gmail.com>
2019-11-03 12:15:08 +0600

    Send mail using java mail api without intent

---
+++ app/src/main/java/com/teamcreative/javamailapidemo/Utils.java
@@ -0,0 +1,10 @@
+package com.teamcreative.javamailapidemo;
+
+public class Utils {
+
+    //This is your from email
+    public static final String EMAIL = "xxx@gmail.com";
+
+    //This is your from email password
+    public static final String PASSWORD = "xxx";
+}
